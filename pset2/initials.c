002562a04563e80fa3c762c5589b70dba6c051b9
playeeer <playeeer@ukr.net>
2017-09-18 21:43:14 +0300

    The

--- pset2/initials.c
+++ pset2/initials.c
@@ -9,7 +9,7 @@ string s = get_string();
 printf("%c", toupper(s[0]));
     for(int i = 0, n = strlen(s); i < n; i++)
     {
-        if(s[i] == ' ' && s[i+1] != '\0')
+         if(s[i] == ' ' && s[i+1] != '\0')
         {
             printf("%c", toupper(s[i + 1]));
             i++;
